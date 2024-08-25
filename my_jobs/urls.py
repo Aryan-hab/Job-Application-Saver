fc6a8ed794c24006ce8e406a7176c4dd7244354e
Aryan-hab <67195127+Aryan-hab@users.noreply.github.com>
2024-08-25 17:33:10 +0200

    First commit

--- my_jobs/urls.py
+++ my_jobs/urls.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
+
 from applied_jobs import urls as jobs_urls
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('jobs/', include(jobs_urls)),  # Include URLs from the `jobs` app
+    path('', RedirectView.as_view(url='/jobs/create/', permanent=False)),  # Redirect root to jobs/create
+
 ]
