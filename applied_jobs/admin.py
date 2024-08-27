e39c73aff7e0032b607d8e0ad6b0b47be2400dd6
Aryan-hab <67195127+Aryan-hab@users.noreply.github.com>
2024-08-28 01:05:04 +0200

    Expanded list display

--- applied_jobs/admin.py
+++ applied_jobs/admin.py
@@ -4,4 +4,4 @@ from applied_jobs.models import Job
 
 @admin.register(Job)
 class JobsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['company_name', 'title', 'location', 'application_date', 'status']
