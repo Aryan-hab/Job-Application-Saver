908e1621b3c73e518b11974e98974b2abfe77b32
Aryan-hab <67195127+Aryan-hab@users.noreply.github.com>
2024-08-28 01:06:59 +0200

    Added status to Job, modified location field to use cities_light lib

--- applied_jobs/models.py
+++ applied_jobs/models.py
@@ -1,13 +1,24 @@
 from django.db import models
+from cities_light.models import City
+
+
+statuses = [
+    ('PE', 'Pending'),
+    ('INT', 'Interview Invitation'),
+    ('RE', 'Rejected'),
+    ('WI', 'Withdrawn by candidate')
+]
 
 
 class Job(models.Model):
     company_name = models.CharField(max_length=30, null=False, blank=False)
     title = models.CharField(max_length=30, null=False, blank=False)
-    location = models.CharField(max_length=30, null=True, blank=True)
+    location = models.ForeignKey(City, on_delete=models.PROTECT, null=True, blank=True)
+    platform = models.CharField(max_length=30, null=True, blank=False)
     application_date = models.DateTimeField(auto_now_add=False)
+    status = models.CharField(max_length=30, null=False, blank=False, choices=statuses, default='PE')
     description = models.TextField(blank=False, null=False)
 
     def __str__(self):
-        return self.company_name
+        return f"{self.company_name}"
 
